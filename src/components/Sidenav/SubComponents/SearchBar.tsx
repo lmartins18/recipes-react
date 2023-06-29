87240246e3f7d50dc2161dc059ff4c64968b543b
Luis Martins <luismwolf@gmail.com>
2023-06-29 23:15:30 +0100

    bugfix: iOS zoom bugfix         - searchBar now has less padding (iOS bugfix).

--- src/components/Sidenav/SubComponents/SearchBar.tsx
+++ src/components/Sidenav/SubComponents/SearchBar.tsx
@@ -11,7 +11,7 @@ export const SearchBar = () => {
     }
   };
   return (
-    <div className="relative mt-3 m-3 flex flex-col">
+    <div className="relative m-1 flex flex-col">
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg
           className="w-5 h-5 text-gray-500"
@@ -30,7 +30,7 @@ export const SearchBar = () => {
       <input
         type="text"
         id="search-navbar"
-        className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full p-2 pl-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search..."
         onKeyUp={search}
       />
